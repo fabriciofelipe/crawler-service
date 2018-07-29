18083de8ae87e4e703843b9138aaa932332a50a6
fabriciofelipe <fabriciooak@gmail.com>
2018-07-29 00:10:18 -0300

    implementação de metodo bloco do tipo 'image' com várias ocorrências e refactoring classe service

--- src/main/java/servicecrawlerapi/crawler/application/service/CrawlerService.java
+++ src/main/java/servicecrawlerapi/crawler/application/service/CrawlerService.java
@@ -4,24 +4,38 @@ import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.springframework.stereotype.Service;
 import servicecrawlerapi.crawler.domain.Content;
 import servicecrawlerapi.crawler.domain.Feed;
 import servicecrawlerapi.crawler.domain.Item;
 import servicecrawlerapi.crawler.domain.Page;
 
-import javax.lang.model.util.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static servicecrawlerapi.crawler.application.util.HtmlConstants.*;
+
+
+/**
+ * Classe de serviço com crawler para ler feeds
+ * Autor Fabricio Carvalho
+ */
 @Service
 @Slf4j
 public class CrawlerService {
 
 
+    public static final String TEXT_TYPE = "text";
+    public static final String IMAGE_TYPE = "image";
+
+    /**
+     * Metodo para construir crowler a partir de uma determinada Url
+     * @param url
+     * @return
+     */
     public Optional<Page> getFeed(String url) {
 
         List<Feed> feeds = new ArrayList<>();
@@ -29,16 +43,16 @@ public class CrawlerService {
         List<Item> items = new ArrayList<>();
         try {
             Element doc = Jsoup.connect(url).get();
-            doc.select("item").forEach( it -> {
+            doc.select(ITEM_TAG).forEach( it -> {
                 items.add(Item.builder()
-                        .title(it.select("title").text())
-                        .link(it.select("link").text())
-                        .content(builderContent(it.select("description").text()))
+                        .title(it.select(TITLE_TAG).text())
+                        .link(it.select(LINK_TAG).text())
+                        .content(builderContent(it.select(DESCRIPTION_TAG).text()))
                         .build());
             });
             feed.setItem(items);
         } catch (IOException e) {
-            log.error("Erro ao desserelizar Url");
+            log.error("Erro desserializar Url: {}", url);
         }
         feeds.add(feed);
         Optional<Page> page =  Optional.ofNullable(Page.builder().feed(feeds).build());
@@ -48,35 +62,81 @@ public class CrawlerService {
     private List<Content> builderContent(String type) {
         Document doc = Jsoup.parse(type).normalise();
         List<Content> contents = new ArrayList<>();
-
-        doc.select("body").forEach( desc -> {
+        //percorre html gerado a partir da tag description
+        doc.select(BODY_TAG).forEach( desc -> {
+            //percorre tags filhas de body
             desc.childNodes().forEach(bodyChild -> {
+                //percorre tags filhas das filhas de body
                 bodyChild.childNodes().forEach(parentBody -> {
-                    if (bodyChild.nodeName().equals("div") && parentBody.nodeName().equals("img")){
-                        List<String> src = new ArrayList<>();
-                        Content content = Content.builder().type("image").build();
-                        src.add(parentBody.attr("src"));
-                        content.setContent(src);
-                        contents.add(content);
-                    }
 
-                    if(bodyChild.nodeName().equals("p") && !parentBody.toString().equals(" &nbsp;")){
+                    builderContentTagImg(contents, bodyChild, parentBody);
 
-                        List<String> src = new ArrayList<>();
-                        Content content = Content.builder().type("text").build();
-                        if(parentBody instanceof Element){
-                            src.add(((Element) parentBody).select(((Element) parentBody).tagName()).text());
-                        } else {
-                            src.add(parentBody.toString());
-                        }
-                        content.setContent(src);
-                        contents.add(content);
-                    }
+                    builderContentTagUl(contents, bodyChild, parentBody);
+
+                    builderContentTagP(contents, bodyChild, parentBody);
                 });
             });
         });
 
         return contents;
+    }
+
+    /**
+     * Metodo de construção do bloco do tipo 'text'
+     * @param contents
+     * @param bodyChild
+     * @param parentBody
+     */
+    private void builderContentTagP(List<Content> contents, Node bodyChild, Node parentBody) {
+        if(bodyChild.nodeName().equals(P_TAG) && !parentBody.toString().equals(" &nbsp;") && !"".equals(parentBody.toString().trim())){
+
+            List<String> value = new ArrayList<>();
+            Content content = Content.builder().type(TEXT_TYPE).build();
+            if(parentBody instanceof Element){
+                value.add(((Element) parentBody).select(((Element) parentBody).tagName()).text());
+            } else {
+                value.add(parentBody.toString().replaceAll("&nbsp;", "").trim());
+            }
+            content.setContent(value);
+            contents.add(content);
+        }
+    }
+
+    /**
+     * Metodo de construção do bloco do tipo 'image' com várias ocorrências
+     * @param contents
+     * @param bodyChild
+     * @param parentBody
+     */
+    private void builderContentTagUl(List<Content> contents, Node bodyChild, Node parentBody) {
+        if (bodyChild.nodeName().equals(DIV_TAG) && parentBody.nodeName().equals(UL_TAG)){
+            List<String> href = new ArrayList<>();
+            Content content = Content.builder().type(IMAGE_TYPE).build();
+            parentBody.childNodes().forEach(childParentBody -> {
+                if(childParentBody instanceof Element) {
+                    href.add(((Element) childParentBody).children().attr(HREF_ATT));
+                }
+            });
+            content.setContent(href);
+            contents.add(content);
+        }
+    }
+
+    /**
+     * Metodo de construção do bloco do tipo 'image' com uma ocorrência
+     * @param contents
+     * @param bodyChild
+     * @param parentBody
+     */
+    private void builderContentTagImg(List<Content> contents, Node bodyChild, Node parentBody) {
+        if (bodyChild.nodeName().equals(DIV_TAG) && parentBody.nodeName().equals(IMG_TAG)){
+            List<String> src = new ArrayList<>();
+            Content content = Content.builder().type(IMAGE_TYPE).build();
+            src.add(parentBody.attr(SRC_ATT));
+            content.setContent(src);
+            contents.add(content);
+
+        }
     }
 
 }
---
+++ src/main/java/servicecrawlerapi/crawler/application/util/HtmlConstants.java
@@ -0,0 +1,17 @@
+package servicecrawlerapi.crawler.application.util;
+
+public class HtmlConstants {
+
+    public static final String P_TAG = "p";
+    public static final String DIV_TAG = "div";
+    public static final String IMG_TAG = "img";
+    public static final String UL_TAG = "ul";
+    public static final String BODY_TAG = "body";
+    public static final String ITEM_TAG = "item";
+    public static final String TITLE_TAG = "title";
+    public static final String LINK_TAG = "link";
+    public static final String DESCRIPTION_TAG = "description";
+    public static final String HREF_ATT = "href";
+    public static final String SRC_ATT = "src";
+
+}
